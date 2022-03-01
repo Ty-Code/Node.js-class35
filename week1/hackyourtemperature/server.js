0ba6869ee1da6f9ab3a825a9d33e9486f23d3bcc
Talha Y <tycode0@gmail.com>
2022-03-01 12:00:54 +0100

    finished project for homework week1

---
+++ week1/hackyourtemperature/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!');
+});
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server is on Port ${PORT}`));
